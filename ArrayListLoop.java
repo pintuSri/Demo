082e0d0fc62fed0463da2c4ffd4a28dc79e6f6ed
Sridhar <interiaengg@gmail.com>
2024-11-22 10:12:13 +0530

    Adding ArrayList

---
+++ ArrayListLoop.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class ArrayListLoop{
+	public static void main(String[] args){
+		ArrayList<String> names = new ArrayList<>();
+		names.add("Sridhar");
+		names.add("Shankar");
+		names.add("Sharan");
+		names.add("Shiva");
+		names.add("Shaara");
+		System.out.println(names);
+	}
+}
